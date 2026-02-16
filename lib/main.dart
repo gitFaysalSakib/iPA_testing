a529ea13f7926f3e4b2687a2c512803ca52ecbcb
faysal kabir <faysalsakib.409@gmail.com>
2026-02-16 14:14:36 +0600

    ios file update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
+import 'package:appsonair_flutter_appsync/app_sync_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  @override
+  void initState() {
+    AppSyncService.sync(context);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
